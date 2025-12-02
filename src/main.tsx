95a8c0a4c9e2889cc697f9c0dba0ba882ca802b1
Alassan-alt <hassanebah345@gmail.com>
2025-12-02 10:50:08 +0000

    dernier commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import { AuthProvider } from './contexts/AuthContext'
+import App from './App.jsx'
+import { AuthProvider } from './contexts/AuthContext.jsx'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
